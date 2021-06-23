2e0029ae04c6d4fb240faad0e5990e503ecb00bb
thanhngo2345 <72295938+thanhngo2345@users.noreply.github.com>
2021-06-23 17:40:21 -0500

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-    printf("Hello World - First Test - GitHub - This is MAIN branch.  \n");    
+    printf("Hello World - First Test - GitHub - This is MAIN branch.  \n"); 
+    printf("Hello World - First Test - GitHub - I am testing GitHub on June 23, 2021  \n");  
 
     return 0;
 }
